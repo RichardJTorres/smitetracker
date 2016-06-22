8aec23c06811f2e2cdb18556c6ef5d053d99146e
Rich Torres <richjtorres@gmail.com>
2016-06-22 00:53:16 -0700

    adding flask security models

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, login_required
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+from models import User, Role
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='matt@nobien.net', password='password')
+    db.session.commit()
+
+
+@login_required
 @app.route('/')
 def index():
     return 'Hello World'
