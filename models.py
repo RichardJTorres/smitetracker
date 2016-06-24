c1b09909471e01f5e1206b97d6f683074b93b7a0
Rich Torres <richjtorres@gmail.com>
2016-06-24 00:22:49 -0700

    adding player model and migration

--- models.py
+++ models.py
@@ -20,3 +20,21 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    avatar_url = db.Column(db.String)
+    created = db.Column(db.DateTime)
+    last_login = db.Column(db.DateTime)
+    leaves = db.Column(db.Integer)
+    level = db.Column(db.Integer)
+    losses = db.Column(db.Integer)
+    mastery_level = db.Column(db.Integer)
+    name = db.Column(db.String)
+    personal_status_message = db.Column(db.String)
+    total_achievements = db.Column(db.Integer)
+    total_worshipers = db.Column(db.Integer)
+    wins = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Player {0}'.format(self.name)
